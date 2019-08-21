b9186f567682107e6adcf6b5fd22831473f537e8
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-08-21 14:13:40 -0300

    Create conversa.bat

---
+++ conversa.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set CLASSPATH="lib\conversa.jar;lib\ditac.jar;lib\dtd.jar;lib\flexmark.jar;lib\json.jar;lib\jsoup-1.11.3.jar;lib\mapdb.jar;lib\openxliff.jar;lib\relaxng.jar;lib\resolver.jar;lib\saxon9.jar;lib\snowball.jar;lib\whcmin.jar;lib\xslthl;lib\win64\swt.jar"
+
+javaw  -cp %CLASSPATH% com.maxprograms.conversa.Conversa
+
+
