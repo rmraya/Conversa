2c5e430abefe2bd349351522ad6d3f865f1112e9
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-08-21 14:26:11 -0300

    Ignored JRE and output

--- conversa.bat
+++ conversa.bat
@@ -2,6 +2,6 @@
 
 set CLASSPATH="lib\conversa.jar;lib\ditac.jar;lib\dtd.jar;lib\flexmark.jar;lib\json.jar;lib\jsoup-1.11.3.jar;lib\mapdb.jar;lib\openxliff.jar;lib\relaxng.jar;lib\resolver.jar;lib\saxon9.jar;lib\snowball.jar;lib\whcmin.jar;lib\xslthl;lib\win64\swt.jar"
 
-javaw  -cp %CLASSPATH% com.maxprograms.conversa.Conversa
+start .\jre\bin\javaw.exe  -cp %CLASSPATH% com.maxprograms.conversa.Conversa
 
 
