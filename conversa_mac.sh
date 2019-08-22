eefe2ffcf5dfea15df67c50932d2d426ae651fb3
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-08-22 14:39:39 -0300

    Updated shell scripts

--- conversa_mac.sh
+++ conversa_mac.sh
@@ -3,13 +3,7 @@
 cd "$(dirname "$0")/"
 
 OPTIONS=" -Xmx1500m -Xdock:name=Conversa -XstartOnFirstThread "
-
 CLASSPATH="lib/conversa.jar:lib/ditac.jar:lib/dtd.jar:lib/flexmark.jar:lib/json.jar:lib/jsoup-1.11.3.jar:lib/mapdb.jar:lib/openxliff.jar:lib/relaxng.jar:lib/resolver.jar:lib/saxon9.jar:lib/snowball.jar:lib/whcmin.jar:lib/xslthl:lib/mac64/swt.jar"
-
-
 JAVA=./jre/bin/java
 
 ${JAVA} ${OPTIONS} -cp ${CLASSPATH} com.maxprograms.conversa.Conversa
-
-
-
