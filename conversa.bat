3c702628d37894909040b24de9d359ee00bb1db2
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-08-23 11:36:35 -0300

    Updated launcher scripts

--- conversa.bat
+++ conversa.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CLASSPATH="lib\conversa.jar;lib\ditac.jar;lib\dtd.jar;lib\flexmark.jar;lib\json.jar;lib\jsoup-1.11.3.jar;lib\mapdb.jar;lib\openxliff.jar;lib\relaxng.jar;lib\resolver.jar;lib\saxon9.jar;lib\snowball.jar;lib\whcmin.jar;lib\xslthl;lib\win64\swt.jar"
+set CLASSPATH="lib\conversa.jar;lib\ditac.jar;lib\flexmark.jar;lib\mapdb.jar;lib\openxliff.jar;lib\relaxng.jar;lib\resolver.jar;lib\saxon9.jar;lib\snowball.jar;lib\whcmin.jar;lib\xslthl;lib\win64\swt.jar"
 
 start .\jre\bin\javaw.exe  -cp %CLASSPATH% com.maxprograms.conversa.Conversa
 
