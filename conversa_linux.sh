194d58a3c561cd71e3de5e13a8318b79adca30a0
Rodolfo M. Raya <rmraya@maxprograms.com>
2022-09-04 19:22:55 -0300

    Added linux launcher script

--- conversa_linux.sh
+++ conversa_linux.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 cd "$(dirname "$0")/"
 
-OPTIONS=" -Xmx1500m"
+OPTIONS=" -Xmx1500m "
 CLASSPATH="lib/conversa.jar:lib/ditac.jar:lib/flexmark.jar:lib/json.jar:lib/openxliff.jar:lib/relaxng.jar:lib/resolver.jar:lib/saxon10.jar:lib/snowball.jar:lib/whcmin.jar:lib/xslthl:lib/swt.jar"
-JAVA=./jre/bin/java
+export JAVA_HOME="$PWD/jre/"
+JAVA="$JAVA_HOME/bin/java"
 
 ${JAVA} ${OPTIONS} -cp ${CLASSPATH} com.maxprograms.conversa.Conversa
